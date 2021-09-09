dfa1763e43622de2ccc5dd9873c582bd1648c38c
Tahmid <mdtahmidh7@gmail.com>
2021-09-09 13:28:55 +0600

    Dynamic Sub String Option is Added and Reading DNA Sequence from  text file is also added.

--- src/com/company/Huntingtons.java
+++ src/com/company/Huntingtons.java
@@ -1,23 +1,60 @@
 package com.company;
-
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Huntingtons {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String DnaSequence="";
 	String mainString = "wAABABCABabaBDnHjABsssabAbaBABAbsababAbaBABAb";
 	String subString = "AB";
 	int count =0,maxCount=0;
         mainString = mainString.toUpperCase();
         subString = subString.toUpperCase();
 
+
+      /*  -------------------File Reading-------------*/
+        /* ____________Reading File_______________*/
+
+        File file = new File("sequences\\1.txt");
+        Scanner fileScanner = new Scanner(file);
+        /*String DnaSequence = "";*/
+        while (fileScanner.hasNextLine()) {
+            DnaSequence = DnaSequence.concat(fileScanner.nextLine());
+            //  System.out.println(fileScanner.nextLine()+"\n");
+        }
+        System.out.println(DnaSequence);
+
+
+
         System.out.println("   -----    ");
-        for (int i=0,j=2;j<=mainString.length();i++,j++){
+
+
+
+        mainString = DnaSequence;
+        System.out.println(mainString);
+        System.out.println("Enter DNA Sub-Sequence : ");
+        Scanner subStingScanner = new Scanner(System.in);
+        String s = subStingScanner.nextLine();
+        s = s.toUpperCase();
+        int subStringLength = s.length();
+        System.out.println("Sub string length is "+subStringLength);
+        System.out.println("DNA sub sequence is : " + s);
+        subString=s;
+        System.out.println("\nSUB String is = "+subString);
+
+
+
+
+
+        for (int i=0,j=subStringLength;j<=mainString.length();i++,j++){
             String temp = mainString.substring(i,j);
             System.out.println(temp);
             if (temp.equals(subString)){
                 count++;
                 //System.out.println(count+" Match Found");
-                for (i=i+2,j=j+2;j<=mainString.length();i=i+2,j=j+2){
+                for (i=i+subStringLength,j=j+subStringLength;j<=mainString.length();i=i+subStringLength,j=j+subStringLength){
                     String temp1 = mainString.substring(i,j);
                     if (temp1.equals(subString)){
                         count++;
